160428f4ffdce487a3953a58bc977c2eeb8d661a
shelton15 <88257757+shelton15@users.noreply.github.com>
2023-08-24 21:43:00 +0100

    api calls added

---
+++ Netflix Clone/Managers/APICaller.swift
@@ -0,0 +1,19 @@
+//
+//  APICaller.swift
+//  Netflix Clone
+//
+//  Created by imac on 24/08/2023.
+//
+
+import Foundation
+
+
+struct Constants {
+    static let API_key = "5bd182c011cd64eb50095c5e7d4ce891"
+}
+
+class APICaller {
+    static let shared = APICaller()
+    
+    
+}
