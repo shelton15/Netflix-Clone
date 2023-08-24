8a5cda0cd8988dc77313be98d499fe5bc56bc774
shelton15 <88257757+shelton15@users.noreply.github.com>
2023-08-24 23:44:19 +0100

    establishing connections and fetching data

--- Netflix Clone/Managers/APICaller.swift
+++ Netflix Clone/Managers/APICaller.swift
@@ -13,12 +13,16 @@ struct Constants {
     static let baseURL = "https://api.themoviedb.org"
 }
 
+enum APIError: Error {
+    case failedTogetData
+}
+
 class APICaller {
     static let shared = APICaller()
     
     
     
-    func getTrendingMovies(completion: @escaping (String) -> Void) {
+    func getTrendingMovies(completion: @escaping (Result<[Movie], Error>) -> Void) {
         
         guard let url = URL(string: "\(Constants.baseURL)/3/trending/all/day?api_key=\(Constants.API_key)") else {return}
         
@@ -28,11 +32,11 @@ class APICaller {
             }
             
             do {
-                let results = try JSONSerialization.jsonObject(with: data, options: .fragmentsAllowed)
-                print(results)
+                let results = try JSONDecoder().decode(TrendingMoviesResponse.self, from: data)
+                completion(.success(results.results))
                 
             } catch {
-                print(error.localizedDescription)
+                completion(.failure(error))
             }
             
         }
---
+++ Netflix Clone/Models/Movie.swift
@@ -0,0 +1,48 @@
+//
+//  Movie.swift
+//  Netflix Clone
+//
+//  Created by imac on 24/08/2023.
+//
+
+import Foundation
+
+struct TrendingMoviesResponse: Codable {
+    let results: [Movie]
+}
+
+struct Movie: Codable {
+    let id: Int
+    let media_type: String?
+    let original_name: String?
+    let original_title: String?
+    let poster_path: String?
+    let overview: String?
+    let vote_count: Int
+    let release_date: String?
+    let vote_average: Double
+}
+
+
+/*
+ adult = 0;
+ "backdrop_path" = "/lbJG35jQE6erMW68YmQ1moE8TZQ.jpg";
+ "first_air_date" = "2023-08-22";
+ "genre_ids" =             (
+     10765,
+     10759
+ );
+ id = 114461;
+ "media_type" = tv;
+ name = Ahsoka;
+ "origin_country" =             (
+     US
+ );
+ "original_language" = en;
+ "original_name" = Ahsoka;
+ overview = "Former Jedi Knight Ahsoka Tano investigates an emerging threat to a vulnerable galaxy.";
+ popularity = "281.673";
+ "poster_path" = "/laCJxobHoPVaLQTKxc14Y2zV64J.jpg";
+ "vote_average" = "8.362";
+ "vote_count" = 47;
+ */
