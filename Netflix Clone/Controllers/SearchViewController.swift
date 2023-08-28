11050d763c4226f910f48b2b901f156ece1811ac
shelton15 <88257757+shelton15@users.noreply.github.com>
2023-08-28 21:58:56 +0100

    search page begun

--- Netflix Clone/Controllers/SearchViewController.swift
+++ Netflix Clone/Controllers/SearchViewController.swift
@@ -8,9 +8,21 @@
 import UIKit
 
 class SearchViewController: UIViewController {
-
+    
+    
+    private let discoverTable: UITableView = {
+        
+        let table = UITableView()
+        table.register(TitleTableViewCell.self, forCellReuseIdentifier: TitleTableViewCell.identifier)
+        return table
+        
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        title = "Search"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationItem.largeTitleDisplayMode = .always
 
         view.backgroundColor = .systemBackground
     }
