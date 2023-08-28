fccbefa88184732bb55972671de671818f876452
shelton15 <88257757+shelton15@users.noreply.github.com>
2023-08-28 23:17:14 +0100

    implementing search function

--- Netflix Clone/Controllers/General/SearchResultsViewController.swift
+++ Netflix Clone/Controllers/General/SearchResultsViewController.swift
@@ -8,13 +8,30 @@
 import UIKit
 
 class SearchResultsViewController: UIViewController {
+    
+    private let searchResultsCollectionView: UICollectionView = {
+        
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: 100, height: 200)
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.register(TitleCollectionViewCell.self, forCellWithReuseIdentifier: TitleCollectionViewCell.identifier)
+        
+        return collectionView
+        
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .systemRed
+        view.backgroundColor = .systemBackground
+        view.addSubview(searchResultsCollectionView)
     }
     
 
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        
+    }
     
 }
