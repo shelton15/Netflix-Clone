ff4fb7a0bd219f6cdae59017ac63d28483116147
shelton15 <88257757+shelton15@users.noreply.github.com>
2023-08-28 22:47:06 +0100

    folder rearrange

---
+++ Netflix Clone/Controllers/SearchResultsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SearchResultsViewController.swift
+//  Netflix Clone
+//
+//  Created by imac on 28/08/2023.
+//
+
+import UIKit
+
+class SearchResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemRed
+    }
+    
+
+    
+}
