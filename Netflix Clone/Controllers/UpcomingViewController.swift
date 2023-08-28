fab0246b797ade48d1c1370b4afc9cd01c965efa
shelton15 <88257757+shelton15@users.noreply.github.com>
2023-08-28 19:02:53 +0100

    live data added to upcoming screen

--- Netflix Clone/Controllers/UpcomingViewController.swift
+++ Netflix Clone/Controllers/UpcomingViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class UpcomingViewController: UIViewController {
     
+    private var titles: [Title] = [Title]()
+    
     private let upcomingTable: UITableView = {
         
         let table = UITableView()
@@ -37,6 +39,23 @@ class UpcomingViewController: UIViewController {
         
         upcomingTable.frame = view.bounds
     }
+    
+    private func fetchUpcoming() {
+        
+        APICaller.shared.getUpcomingMovies { [weak self] result in
+            switch result {
+            case .success(let titles):
+                self?.titles = titles
+                DispatchQueue.main.async {
+                    self?.upcomingTable.reloadData()
+                }
+                
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+        
+    }
 
 }
 
@@ -44,13 +63,13 @@ class UpcomingViewController: UIViewController {
 extension UpcomingViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return titles.count
     }
     
     func tableView(_ tableVIew: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableVIew.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = "text"
+        cell.textLabel?.text = titles[indexPath.row].original_name ?? titles[indexPath.row].original_title ?? "Unknown"
         
         return cell
     }
