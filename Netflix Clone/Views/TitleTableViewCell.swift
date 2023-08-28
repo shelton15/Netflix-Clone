150eac81ea5407b3cdd5ef76744982d7d315abbf
shelton15 <88257757+shelton15@users.noreply.github.com>
2023-08-28 21:01:05 +0100

    play button added

--- Netflix Clone/Views/TitleTableViewCell.swift
+++ Netflix Clone/Views/TitleTableViewCell.swift
@@ -14,6 +14,7 @@ class TitleTableViewCell: UITableViewCell {
     private let playTitleButton: UIButton = {
         
         let button = UIButton()
+        button.setImage(UIImage(systemName: "play.circle"), for: .normal)
         button.translatesAutoresizingMaskIntoConstraints = false
         
         return button
@@ -62,8 +63,14 @@ class TitleTableViewCell: UITableViewCell {
             titleLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor)
         ]
         
+        let playTitleButtonConstraints = [
+            playTitleButton.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -20),
+            playTitleButton.centerYAnchor.constraint(equalTo: contentView.centerYAnchor)
+        ]
+        
         NSLayoutConstraint.activate(titlesPosterUIImageViewConstraints)
         NSLayoutConstraint.activate(titleLabelConstraints)
+        NSLayoutConstraint.activate(playTitleButtonConstraints)
         
     }
     
