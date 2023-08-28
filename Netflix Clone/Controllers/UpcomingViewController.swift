8008fdb7fa7fd289db8c90e20ef987cf12b57056
shelton15 <88257757+shelton15@users.noreply.github.com>
2023-08-28 18:25:11 +0100

    linking the pages to the navbar

--- Netflix Clone/Controllers/UpcomingViewController.swift
+++ Netflix Clone/Controllers/UpcomingViewController.swift
@@ -8,11 +8,19 @@
 import UIKit
 
 class UpcomingViewController: UIViewController {
+    
+    private let upcomingTable: UITableView = {
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .systemBackground
+        
+        title = "Upcoming"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationItem.largeTitleDisplayMode = .always
     }
     
 
