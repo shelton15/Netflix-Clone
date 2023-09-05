35e27a6f5af89190ee9d9dd690fcc73cbbc8bbe7
shelton15 <88257757+shelton15@users.noreply.github.com>
2023-09-05 20:11:17 +0100

    ...coremodel

---
+++ Netflix Clone/Managers/DataPersistenceManager.swift
@@ -0,0 +1,17 @@
+//
+//  DataPersistenceManager.swift
+//  Netflix Clone
+//
+//  Created by imac on 05/09/2023.
+//
+
+import Foundation
+
+
+class DataPersistenceManager {
+    
+    static let shared = DataPersistenceManager()
+    
+    func
+    
+}
