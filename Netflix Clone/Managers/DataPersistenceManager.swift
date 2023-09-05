83b628ea1ec2f0d0f32a076d329f6ba2c3ba3ea8
shelton15 <88257757+shelton15@users.noreply.github.com>
2023-09-05 20:33:39 +0100

    ....

--- Netflix Clone/Managers/DataPersistenceManager.swift
+++ Netflix Clone/Managers/DataPersistenceManager.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import UIKit
+import CoreData
 
 
 class DataPersistenceManager {
@@ -23,6 +24,23 @@ class DataPersistenceManager {
         
         let context = appDelegate.persistentContainer.viewContext
         
+        let item = TitleItem(context: context)
+        
+        item.original_title = model.original_title
+        item.id = Int64(model.id)
+        item.original_name = model.original_name
+        item.overview = model.overview
+        item.media_type = model.media_type
+        item.poster_path = model.poster_path
+        item.release_date = model.release_date
+        item.vote_count = Int64(model.vote_count)
+        item.vote_average = model.vote_average
+        
+        do {
+            try context.save()
+        } catch {
+            print(error.localizedDescription)
+        }
     }
     
 }
