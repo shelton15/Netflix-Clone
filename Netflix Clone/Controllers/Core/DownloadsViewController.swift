19bd51beea74dcc58b90ac4ef7c96cf62569eca0
shelton15 <88257757+shelton15@users.noreply.github.com>
2023-09-05 23:43:10 +0100

    download page view and fetch function

--- Netflix Clone/Controllers/Core/DownloadsViewController.swift
+++ Netflix Clone/Controllers/Core/DownloadsViewController.swift
@@ -8,6 +8,16 @@
 import UIKit
 
 class DownloadsViewController: UIViewController {
+    
+    private var titles: [TitleItem] = [TitleItem]()
+    
+    private let downloadedTable: UITableView = {
+        
+        let table = UITableView()
+        table.register(TitleTableViewCell.self, forCellReuseIdentifier: TitleTableViewCell.identifier)
+        return table
+        
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,9 +27,62 @@ class DownloadsViewController: UIViewController {
         title = "Downloads"
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationController?.navigationItem.largeTitleDisplayMode = .always
+        
+        downloadedTable.delegate = self
+        downloadedTable.dataSource = self
+        
+        fetchLocalStorageForDownload()
+    }
+    
+    
+    private func fetchLocalStorageForDownload() {
+        
+        DataPersistenceManager.shared.fetchingTitlesFromDataBase { [weak self] result in
+            switch result {
+            case .success(let titles):
+                
+                self?.titles = titles
+                
+                self?.downloadedTable.reloadData()
+                
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
     }
     
 
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        downloadedTable.frame = view.bounds
+    }
     
+}
 
+
+extension DownloadsViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return titles.count
+    }
+    
+    func tableView(_ tableVIew: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        guard let cell = tableVIew.dequeueReusableCell(withIdentifier: TitleTableViewCell.identifier, for: indexPath) as? TitleTableViewCell else {
+            return UITableViewCell()
+        }
+        
+        let title = titles[indexPath.row]
+        
+        cell.configure(with: TitleViewModel(titleName: (title.original_title ?? title.original_name) ?? "Unknowm title name", posterURL: title.poster_path ?? ""))
+        
+        return cell
+        
+    }
+    
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150
+    }
 }
--- Netflix Clone/Managers/DataPersistenceManager.swift
+++ Netflix Clone/Managers/DataPersistenceManager.swift
@@ -15,6 +15,7 @@ class DataPersistenceManager {
     enum DatabaseError: Error {
         
         case failedToSaveData
+        case failedToFetchData
         
     }
     
@@ -48,6 +49,31 @@ class DataPersistenceManager {
         } catch {
             completion(.failure(DatabaseError.failedToSaveData))
         }
+    }
+    
+    func fetchingTitlesFromDataBase(completion: @escaping (Result<[TitleItem], Error>) -> Void) {
+        
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            
+            return
+            
+        }
+        
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let request: NSFetchRequest<TitleItem>
+        
+        request = TitleItem.fetchRequest()
+        
+        do {
+            
+            let titles = try context.fetch(request)
+            completion(.success(titles))
+            
+        } catch {
+            completion(.failure(DatabaseError.failedToFetchData))
+        }
+        
     }
     
 }
