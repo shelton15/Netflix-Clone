04c7cf07c605f6093ffb3796abebaf114a178a50
shelton15 <88257757+shelton15@users.noreply.github.com>
2023-09-06 00:18:54 +0100

    completed code project

--- Netflix Clone/Controllers/Core/DownloadsViewController.swift
+++ Netflix Clone/Controllers/Core/DownloadsViewController.swift
@@ -35,6 +35,12 @@ class DownloadsViewController: UIViewController {
         downloadedTable.dataSource = self
         
         fetchLocalStorageForDownload()
+        
+        NotificationCenter.default.addObserver(forName: NSNotification.Name("Downloaded"), object: nil, queue: nil) { _ in
+            
+            self.fetchLocalStorageForDownload()
+            
+        }
     }
     
     
@@ -117,6 +123,31 @@ extension DownloadsViewController: UITableViewDelegate, UITableViewDataSource {
             break;
             
         }
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        let title = titles[indexPath.row]
+        
+        guard let titleName = title.original_title ?? title.original_name else {
+            return
+        }
+        
+        APICaller.shared.getMovie(with: titleName) { [weak self] result in
+            switch result {
+            case .success(let videoElement):
+                DispatchQueue.main.async {
+                    let vc = TitlePreviewViewController()
+                    vc.configure(with: TitlePreviewViewModel(title: titleName, youtubeView: videoElement, titleOverview: title.overview ?? ""))
+                    self?.navigationController?.pushViewController(vc, animated: true)
+                }
+                
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+        
     }
     
 }
